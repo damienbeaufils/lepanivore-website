afca6181ce1ea8d2a168cd33751373bd82f2cc48
Damien Beaufils <damien.beaufils@gmail.com>
2020-07-11 09:41:54 -0400

    🔧 use jest global setup in stryker for timezone

--- back/jest-mutation.config.js
+++ back/jest-mutation.config.js
@@ -6,4 +6,5 @@ module.exports = {
   },
   testEnvironment: 'node',
   testTimeout: 30000,
+  globalSetup: '../../../jest-global-setup.js', // path is 3 directories up, because stryker working directory is back/.stryker-tmp/sandboxXYZ
 };
