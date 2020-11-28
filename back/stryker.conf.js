ccaec0f5ba442fd2f4fdc4837a9723b4c4fc910e
Damien Beaufils <damien.beaufils@gmail.com>
2020-11-28 09:55:26 -0500

    🔧 update Stryker configuration

--- back/stryker.conf.js
+++ back/stryker.conf.js
@@ -2,13 +2,10 @@
  * @type {import('@stryker-mutator/api/core').StrykerOptions}
  */
 module.exports = {
-  mutator: 'typescript',
   packageManager: 'npm',
   reporters: ['html', 'clear-text', 'progress', 'dashboard'],
   testRunner: 'jest',
-  transpilers: [],
   coverageAnalysis: 'off',
-  tsconfigFile: 'tsconfig.json',
   mutate: ['src/**/*.ts', '!src/**/*.spec.ts', '!src/**/*.module.ts'],
   jest: {
     configFile: './jest-mutation.config.js',
