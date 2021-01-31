3f986448a70c20bfeb681ac07ba77f4ba52064de
Damien Beaufils <damien.beaufils@gmail.com>
2021-01-31 13:44:39 -0500

    ✨ add facebook feed on index page
    fixes #86

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,24 +1,30 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-card class="yellow-card">
-        <v-card-title class="headline">Des ingrédients naturels pour un pain exceptionnel.</v-card-title>
-        <v-card-text>
-          <p>Toutes nos pâtes à pain sont exclusivement préparées à partir de farines biologiques, d'eau filtrée, de levain naturel biologique et de sel de mer.</p>
-          <p>Aucun additif n'est utilisé et la teneur en sel est réduite.</p>
-          <p>Nos pains se conservent longtemps, sont consistants (type à l'ancienne) et très protéinés.</p>
-          <p><img class="recipe" alt="Recette Panivore" src="/img/recette-panivore.png" /></p>
-        </v-card-text>
-      </v-card>
+    <v-row>
+      <v-col cols="12" sm="12" md="8">
+        <v-card class="yellow-card">
+          <v-card-title class="headline">Des ingrédients naturels pour un pain exceptionnel.</v-card-title>
+          <v-card-text>
+            <p>Toutes nos pâtes à pain sont exclusivement préparées à partir de farines biologiques, d'eau filtrée, de levain naturel biologique et de sel de mer.</p>
+            <p>Aucun additif n'est utilisé et la teneur en sel est réduite.</p>
+            <p>Nos pains se conservent longtemps, sont consistants (type à l'ancienne) et très protéinés.</p>
+            <p><img class="recipe" alt="Recette Panivore" src="/img/recette-panivore.png" /></p>
+          </v-card-text>
+        </v-card>
 
-      <v-card class="yellow-card">
-        <v-card-title class="headline">Livraison à domicile chaque jeudi soir.</v-card-title>
-        <v-card-text>
-          <p>Vous passez votre commande <nuxt-link to="commander">ici</nuxt-link> ; nous vous la livrons à domicile le jeudi soir.</p>
-          <p><i>Service gratuit pour la Petite-Patrie (Jean Talon - Des Carrières - Des Érables - Christophe-Colomb).</i></p>
-        </v-card-text>
-      </v-card>
-    </v-flex>
+        <v-card class="yellow-card">
+          <v-card-title class="headline">Livraison à domicile chaque jeudi soir.</v-card-title>
+          <v-card-text>
+            <p>Vous passez votre commande <nuxt-link to="commander">ici</nuxt-link> ; nous vous la livrons à domicile le jeudi soir.</p>
+            <p><i>Service gratuit pour la Petite-Patrie (Jean Talon - Des Carrières - Des Érables - Christophe-Colomb).</i></p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="12" md="4">
+        <div class="fb-page" data-href="https://www.facebook.com/lepanivore" data-tabs="timeline" data-height="600"></div>
+      </v-col>
+    </v-row>
   </v-layout>
 </template>
 
