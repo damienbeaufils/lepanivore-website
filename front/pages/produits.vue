07159ee1efd9978050881eb89d0c621793431b22
tmorat <66571543+tmorat@users.noreply.github.com>
2021-08-11 09:39:38 -0400

    Mise à jour des prix de produits

--- front/pages/produits.vue
+++ front/pages/produits.vue
@@ -31,32 +31,32 @@
         <v-col cols="12" sm="6" md="4">
           <h4><u>Tous les jours :</u></h4>
           <ul>
-            <li>La Baguette au levain : 3.30 $</li>
-            <li>La Baguette au levain enrobée au sésame : 3.80 $</li>
+            <li>La Baguette au levain : 3.50 $</li>
+            <li>La Baguette au levain enrobée au sésame : 3.95 $</li>
             <li>L'Exquis <small>(farine de blé tamisée, farine d'épeautre, farine de khorasan, graines de tournesol
               dedans
-              et dessus)</small> : 6.70 $
+              et dessus)</small> : 6.90 $
             </li>
-            <li>Le Moelleux <small>(farine blanche, farine de blé tamisée, lait, huile de canola)</small> : 5.80 $</li>
-            <li>Le Seigle <small>(farine de blé tamisée, farine de seigle)</small> : 5.65 $</li>
-            <li>La Miche Blanche <small>(farine blanche)</small> : 4.35 $</li>
-            <li>La Miche Blanche Double <small>(farine blanche)</small> : 7.85 $</li>
+            <li>Le Moelleux <small>(farine blanche, farine de blé tamisée, lait, huile de canola)</small> : 5.95 $</li>
+            <li>Le Seigle <small>(farine de blé tamisée, farine de seigle)</small> : 5.80 $</li>
+            <li>La Miche Blanche <small>(farine blanche)</small> : 4.50 $</li>
+            <li>La Miche Blanche Double <small>(farine blanche)</small> : 8.10 $</li>
           </ul>
         </v-col>
 
         <v-col cols="12" sm="6" md="4">
           <h4><u>Mardi :</u></h4>
           <ul>
-            <li>L'Avoine <small>(farine de blé tamisée, farine d'avoine)</small> : 5.70 $</li>
+            <li>L'Avoine <small>(farine de blé tamisée, farine d'avoine)</small> : 5.85 $</li>
             <li>Le Multigrains <small>(farine de blé tamisée, flocons d'avoine, graines de tournesol rôties, graines de
-              lin, graines de sésame, graines de citrouilles vertes)</small> : 6.90 $
+              lin, graines de sésame, graines de citrouilles vertes)</small> : 7.10 $
             </li>
           </ul>
 
           <h4><u>Mercredi :</u></h4>
           <ul>
-            <li>Le Khorasan <small>(farine de blé tamisée, farine de khorasan)</small> : 5.70 $</li>
-            <li>Le Datte choco <small>(farine de blé tamisée, dattes séchées, pépites de chocolat)</small> : 7.00 $</li>
+            <li>Le Khorasan <small>(farine de blé tamisée, farine de khorasan)</small> : 5.85 $</li>
+            <li>Le Datte choco <small>(farine de blé tamisée, dattes séchées, pépites de chocolat)</small> : 7.20 $</li>
           </ul>
 
           <h4><u>Jeudi :</u></h4>
@@ -70,17 +70,17 @@
         <v-col cols="12" sm="6" md="4">
           <h4><u>Vendredi :</u></h4>
           <ul>
-            <li>Le Jeremy <small>(farine de blé tamisée, banane fraiche, pépites de chocolat)</small> : 7.00 $</li>
-            <li>Le Noix <small>(farine de blé tamisée, noix de Grenoble)</small> : 6.70 $</li>
+            <li>Le Jeremy <small>(farine de blé tamisée, banane fraiche, pépites de chocolat)</small> : 7.20 $</li>
+            <li>Le Noix <small>(farine de blé tamisée, noix de Grenoble)</small> : 6.90 $</li>
           </ul>
 
           <h4><u>Samedi :</u></h4>
           <ul>
-            <li>Le Datte Noix <small>(farine de blé tamisée, dattes séchées, noix de Grenoble)</small> : 7.70 $</li>
-            <li>Le Raisin Choco <small>(farine de blé tamisée, raisins sultana, pépites de chocolat)</small> : 7.00 $
+            <li>Le Datte Noix <small>(farine de blé tamisée, dattes séchées, noix de Grenoble)</small> : 7.90 $</li>
+            <li>Le Raisin Choco <small>(farine de blé tamisée, raisins sultana, pépites de chocolat)</small> : 7.20 $
             </li>
-            <li>L'Épeautre <small>(farine de blé tamisée, farine d'épeautre)</small> : 5.70 $</li>
-            <li>L'Intégral <small>(farine de blé intégrale)</small> : 5.65 $</li>
+            <li>L'Épeautre <small>(farine de blé tamisée, farine d'épeautre)</small> : 5.85 $</li>
+            <li>L'Intégral <small>(farine de blé intégrale)</small> : 5.80 $</li>
           </ul>
         </v-col>
       </v-row>
@@ -89,48 +89,47 @@
         <v-col cols="12" sm="6" md="4">
           <h4><u>Sur commande :</u></h4>
           <ul>
-            <li>L'Enrobé Sésame<small>(farine de blé tamisée, croûte de graines de sésame)</small> : 7.00 $</li>
-            <li>La Miche Rustique, cuite sur pierre naturelle <small>(farine de blé tamisée)</small> : 5.20 $</li>
-            <li>Le Mariko <small>(farine de blé tamisée, écorces d'orange bio confites)</small> : 7.80 $</li>
+            <li>L'Enrobé Sésame<small>(farine de blé tamisée, croûte de graines de sésame)</small> : 7.20 $</li>
+            <li>La Miche Rustique, cuite sur pierre naturelle <small>(farine de blé tamisée)</small> : 5.40 $</li>
+            <li>Le Mariko <small>(farine de blé tamisée, écorces d'orange bio confites)</small> : 8.05 $</li>
             <li>Le Mariko Choco <small>(farine de blé tamisée, écorces d'orange bio confites et pépites de
-              chocolat)</small> : 8.45 $
+              chocolat)</small> : 8.70 $
             </li>
-            <li>Le Brasseur <small>(farine de blé tamisée, orge malté qui a servi à brasser des bières)</small> : 6.80 $
             </li>
-            <li>Le Canneberge <small>(farine de blé tamisée, canneberges séchées)</small> : 6.70 $</li>
-            <li>Le Raisin <small>(farine de blé tamisée, raisins sultana)</small> : 6.70 $</li>
-            <li>Le Datte <small>(farine de blé tamisée, dattes séchées)</small> : 6.70 $</li>
-            <li>Le Pois Chiche <small>(farine de blé tamisée, farine de pois chiche)</small> : 7.00 $</li>
-            <li>Le Quinoa <small>(farine de blé tamisée, farine de quinoa)</small> : 7.00 $</li>
-            <li>Le Sarrasin <small>(farine de blé tamisée, farine de sarrasin)</small> : 7.00 $</li>
-            <li>Le Maïs <small>(farine blanche, farine de maïs entière)</small> : 5.90 $</li>
-            <li>Le Tordu, pain torsadé cuit sur pierre naturelle <small>(farine blanche)</small> : 4.75 $</li>
+            <li>Le Canneberge <small>(farine de blé tamisée, canneberges séchées)</small> : 6.90 $</li>
+            <li>Le Raisin <small>(farine de blé tamisée, raisins sultana)</small> : 6.90 $</li>
+            <li>Le Datte <small>(farine de blé tamisée, dattes séchées)</small> : 6.90 $</li>
+            <li>Le Pois Chiche <small>(farine de blé tamisée, farine de pois chiche)</small> : 7.20 $</li>
+            <li>Le Quinoa <small>(farine de blé tamisée, farine de quinoa)</small> : 7.20 $</li>
+            <li>Le Sarrasin <small>(farine de blé tamisée, farine de sarrasin)</small> : 7.20 $</li>
+            <li>Le Maïs <small>(farine blanche, farine de maïs entière)</small> : 6.10 $</li>
+            <li>Le Tordu, pain torsadé cuit sur pierre naturelle <small>(farine blanche)</small> : 4.90 $</li>
           </ul>
           <i>Pour une commande des pains suivants, s'ils le sont un autre jour que leur jour de production régulier ou
             le
-            jeudi, alors il faut en commander un mimimum de deux : L'Avoine, Le Khorasan, L'intégral, l'Épeautre, Le
+            jeudi, alors il faut en commander un mimimum de deux : l'Avoine, le Khorasan, l'Intégral, l'Épeautre, le
             Pois
-            Chiche, Le Quinoa, Le Maïs et La Miche Rustique.</i>
+            Chiche, le Quinoa, le Maïs, le Sarrasin et La Miche Rustique.</i>
         </v-col>
 
         <v-col cols="12" sm="6" md="4">
           <h4><u>Les à côtés sur commande :</u></h4>
           <ul>
-            <li>Une pâte à pizza : 3.50 $</li>
-            <li>Deux pâtes à pizza : 6.00 $</li>
+            <li>Une pâte à pizza : 3.75 $</li>
+            <li>Deux pâtes à pizza : 6.50 $</li>
           </ul>
           <ul>
-            <li>2 grands cercles de pâte feuilletée : 12.00 $</li>
-            <li>4 grands cercles de pâte feuilletée : 20.00 $</li>
+            <li>2 grands cercles de pâte feuilletée : 13.00 $</li>
+            <li>4 grands cercles de pâte feuilletée : 22.00 $</li>
           </ul>
           <i>Pâte à pizza comme pâte feuilletée peuvent se congeler car elles sont faites fraiches du jour.<br/>Les pâtes
             feuilletées doivent être commandées au plus tard le mardi soir à 19:00.</i>
           <ul>
-            <li>La douzaine de Gougères <small>(pâte à choux et cheddar vieilli)</small> : 12.00 $</li>
+            <li>La douzaine de Gougères <small>(pâte à choux et cheddar vieilli)</small> : 12.50 $</li>
           </ul>
           <ul>
             <li>
-              La Fougasse <small>(pâte à pain à la levure et différentes garnitures)</small> : 5.45 $
+              La Fougasse <small>(pâte à pain à la levure et différentes garnitures)</small> : 5.65 $
               <ul>
                 <li>olives noires et origan</li>
                 <li>cheddar vieilli et origan</li>
