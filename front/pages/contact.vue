977f089a246062374d7f68e2d315ad04f680b872
tmorat <66571543+tmorat@users.noreply.github.com>
2022-02-20 12:45:31 -0500

    Fermeture pour la relâche 2022

--- front/pages/contact.vue
+++ front/pages/contact.vue
@@ -14,7 +14,12 @@
             Courriel : <a href="mailto:info@lepanivore.com">info@lepanivore.com</a>
           </address>
         </v-col>
-
+        
+         <v-col cols="12" sm="8" md="6">
+          <h3>Semaine de relâche 2022</h3>
+          Veuillez prendre note que la boulangerie sera <b>fermée du 27 Février au 7 Mars inclus.</b><br />Nous serons de retour le <b>mardi 8 mars dès 11:00</b>.
+        </v-col>
+        
         <v-col cols="12" sm="8" md="6">
           <h3>Heures d'ouverture</h3>
           <table>
