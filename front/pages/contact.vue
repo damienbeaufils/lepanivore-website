348b2bd816e401d168aad486eaa5553ca1383b85
GE0RG11 <100042702+GE0RG11@users.noreply.github.com>
2023-06-02 20:27:50 -0400

    contact.vue

--- front/pages/contact.vue
+++ front/pages/contact.vue
@@ -14,11 +14,6 @@
           </address>
         </v-col>
         
-        <v-col cols="12" sm="8" md="6">
-          <h3>Relâche scolaire 2023</h3>
-          Veuillez prendre note que la boulangerie sera <b>fermée du 26 Février au 6 Mars.</b>        
-        </v-col>
-        
          <v-col cols="12" sm="8" md="6">
           <h3>Heures d'ouverture</h3>
           <table>
